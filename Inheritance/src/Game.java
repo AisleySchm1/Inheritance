cc75e12d3c244cdd08df248bef3c3200e78242d1
aisley.schmeisser <aisley.schmeisser@MU-C1-16.mullen.pvt.k12.co.us>
2025-02-18 13:29:09 -0700

    what just happened

--- Inheritance/src/Game.java
+++ Inheritance/src/Game.java
@@ -1,5 +1,18 @@
 
-public class Game
+public abstract class Game
+
 	{
+
+public abstract  void readRules();
+	
+public void winGame()
+{
+	System.out.println("I win!");	
+}
+
+public void countPlayers()
+{
+	System.out.println("There are 4 players playing.");
+}
 
 	}

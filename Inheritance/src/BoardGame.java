55247598ca1692a9367be45e05709f9d978e12f7
aisley.schmeisser <aisley.schmeisser@MU-C1-16.mullen.pvt.k12.co.us>
2025-02-18 13:28:45 -0700

    Project is about halfway through and should focus on doing super and casting

---
+++ Inheritance/src/BoardGame.java
@@ -0,0 +1,23 @@
+
+public class BoardGame extends Game
+	{
+public void winGame()
+{
+	System.out.println("Take that suckers!");
+}
+
+public void rollDice()
+{
+	System.out.println ("I rolled a six!");
+}
+
+public void readRules()
+{
+	System.out.println("On the back of the box.");
+}
+
+public void countPlayers()
+{
+	System.out.println("There are 6 players playing.");
+}
+	}
